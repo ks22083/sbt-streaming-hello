3074329cee9790937d7ef79228ec781492c5a466
ks22083 <ks22083>
2018-01-18 19:59:50 +0300

    Added checkpoint for streaming context

--- src/main/scala/bigdata/streaming/spark/SparkStreamingWordCount.scala
+++ src/main/scala/bigdata/streaming/spark/SparkStreamingWordCount.scala
@@ -6,22 +6,34 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object SparkStreamingWordCount {
 
-  def main(args: Array[String]): Unit = {
+  def createStreamingContext(checkpointDir: String): StreamingContext = {
     val conf = new SparkConf()
-      .setMaster("local[2]")
       .setAppName("StreamingWordCount")
+      .setMaster("local[2]")
+      .set("spark.streaming.backpressure.enabled", "true")
+      .set("spark.streaming.stopGracefullyOnShutdown", "true")
+
     val ssc: StreamingContext = new StreamingContext(conf, Seconds(5))
 
-    //ssc.sparkContext.
+    ssc.checkpoint(checkpointDir)
+    ssc
+  }
 
-    val lines: ReceiverInputDStream[String] = ssc.socketTextStream("localhost", 9999)
+  def main(args: Array[String]): Unit = {
+
+    val checkpointDir = "./checkpoint"
+
+    val streamingContext =
+      StreamingContext.getOrCreate(checkpointDir, () => createStreamingContext(checkpointDir) )
+
+    val lines: ReceiverInputDStream[String] = streamingContext.socketTextStream("localhost", 9999)
     val words: DStream[String] = lines.flatMap( _.split("\\s"))
     val pairs: DStream[(String, Int)] = words.map(word => (word, 1))
     val wordCounts: DStream[(String, Int)] = pairs.reduceByKey( _ + _ )
     wordCounts.print()
 
-    ssc.start()
-    ssc.awaitTermination()
+    streamingContext.start()
+    streamingContext.awaitTermination()
     //ssc.awaitTerminationOrTimeout(20000)
   }
 }
