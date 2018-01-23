7b0df6a29e416ff5cc59ceff84d9a5ee6000ba79
ks22083 <ks22083>
2018-01-23 16:37:22 +0300

    Handle connection. Use scala patern matching.

--- src/main/scala/bigdata/scaping/xml/Simple.scala
+++ src/main/scala/bigdata/scaping/xml/Simple.scala
@@ -1,11 +1,17 @@
 package bigdata.scaping.xml
 
 import java.io.{ByteArrayInputStream, InputStreamReader}
+import java.net.{SocketTimeoutException, UnknownHostException}
 
-import scala.xml.{Node, NodeSeq, XML}
+import scala.xml.{Node, NodeSeq, PrettyPrinter, XML}
 import scalaj.http.Http
 
 object Simple {
+
+  case class ChannelInfo(title: String, link: String,
+                         description: String, language: String,
+                         imageURL:String
+                        )
 
   case class Record(title: String,
                     link: String,
@@ -16,7 +22,6 @@ object Simple {
                     pubDate: Option[String]
                    )
 
-  // todo handle XML escape characters like &amp; &quot; &apos; &lt; &gt;
   def processItemNode(n: Node): Record = {
     Record((n \\ "title").text,
       (n \\ "link").text,
@@ -28,6 +33,7 @@ object Simple {
     )
   }
 
+  // todo handle XML escape characters like &amp; &quot; &apos; &lt; &gt;
   def main(args: Array[String]): Unit = {
 //    val response = Http("https://rg.ru/xml/index.xml")
 //    val response = Http("https://russian.rt.com/rss")
@@ -36,22 +42,28 @@ object Simple {
 //    val response = Http("http://www.fontanka.ru/fontanka.rss")
     val response = Http("http://www.fontanka.ru/fontanka.rss")
     .timeout(connTimeoutMs = 3000, readTimeoutMs = 10000)
-//    .asString
 
-    println(response.asBytes.toString)
-//    println(new String(response.asBytes.body, "Windows-1251"))
-//    val xml = XML.loadString(response.asString.body)
-    val xml = XML.load(new InputStreamReader(new ByteArrayInputStream(response.asBytes.body)))
+    try {
+      response.asBytes.code match {
+        case 200 => println("Ok")
+        case 404 => println(s"Page not found: ${response.url}"); System.exit(1)
+        case n: Int => println(s"${response.asString}\nResponse code: $n"); System.exit(1)
+      }
+    } catch {
+      case e: UnknownHostException => println(s"Host not found: ${e.getMessage}"); System.exit(1)
+      case e: SocketTimeoutException => println(s"$e URL:${response.url}"); System.exit(1)
+    }
 
-    case class ChannelInfo(title: String, link: String,
-                           description: String, language: String,
-                           imageURL:String
-                          )
+    // TODO there should be a way to get encoding from XML declaration
+    val xml = XML.load(new InputStreamReader(new ByteArrayInputStream(response.asBytes.body), "windows-1251"))
+    // TODO transform url with formatter
+    // TODO handle XML escape characters like &amp; &quot; &apos; &lt; &gt;
+    val formatter = new PrettyPrinter(240,4)
 
     val chanNode: NodeSeq = xml \\ "rss" \ "channel"
     chanNode.head.child
       .filter(x => x.label != "item" && !x.isAtom)
-      .foreach(println)
+      .foreach(x => println(formatter.format(x)))
 
     val chanInfo  = ChannelInfo(
       (chanNode \ "title").text,
@@ -64,7 +76,7 @@ object Simple {
     println(chanInfo+"\n")
 
     val itemNodes = xml \\ "item"
-    itemNodes.take(1).foreach(println)
+    itemNodes.take(1).foreach(x => println(formatter.format(x)))
 
     itemNodes
       .take(1)
