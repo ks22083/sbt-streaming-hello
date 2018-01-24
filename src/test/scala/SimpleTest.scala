9a468120177b577e04d8c60f9264aa73ae8a9b74
ks22083 <ks22083>
2018-01-24 19:20:18 +0300

    Extend test template.

--- src/test/scala/SimpleTest.scala
+++ src/test/scala/SimpleTest.scala
@@ -1,11 +1,22 @@
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
-class SimpleTest extends FunSuite {
+// See http://doc.scalatest.org/3.0.1/#org.scalatest.FunSuite
+class SimpleTest extends FunSuite with BeforeAndAfter {
+
+  before {
+
+  }
+
+  after {
+
+  }
 
   test("should pass") {
-    print("test passed")
-    assert(true)
+    println("Test Passed")
+    assert(1 == 1)
   }
+
+  test("Should be implemented") (pending)
 
   ignore("Should be ignored") {
     fail()
