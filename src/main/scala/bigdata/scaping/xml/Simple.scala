82d554a418ee6b3a16ff7354ff5a49a07510ee49
ks22083 <ks22083>
2018-01-24 19:17:27 +0300

    Final hack for encoding.

--- src/main/scala/bigdata/scaping/xml/Simple.scala
+++ src/main/scala/bigdata/scaping/xml/Simple.scala
@@ -1,10 +1,10 @@
 package bigdata.scaping.xml
 
 import java.io.{ByteArrayInputStream, InputStreamReader}
-import java.net.{ConnectException, SocketTimeoutException, UnknownHostException}
+import java.net.{ConnectException, MalformedURLException, SocketTimeoutException, UnknownHostException}
 
 import scala.xml._
-import scalaj.http.Http
+import scalaj.http.{Http, HttpConstants, HttpResponse}
 
 object Simple {
 
@@ -37,43 +37,72 @@ object Simple {
 
   // todo handle XML escape characters like &amp; &quot; &apos; &lt; &gt;
   def main(args: Array[String]): Unit = {
-//    val response = Http("https://rg.ru/xml/index.xml")
-//    val response = Http("https://russian.rt.com/rss")
-//    val response = Http("http://feeds.bbci.co.uk/news/world/rss.xml")
+//    val request = Http("https://rg.ru/xml/index.xml")
+//    val request = Http("https://russian.rt.com/rss")
+//    val request = Http("http://feeds.bbci.co.uk/news/world/rss.xml")
 //  fontanka need conversion from windows-1251
-    val response = Http("http://www.fontanka.ru/fontanka.rss")
-//    val response = Http("http://fontanka.ru/fontanka.rss") // Page moved
-//    val response = Http("http://localhost/fontanka.rss") // Connection refused
+    val request = Http("http://www.fontanka.ru/fontanka.rss")
+//    val request = Http("http://fontanka.ru/fontanka.rss") // Page moved
+//    val request = Http("http://localhost/fontanka.rss") // Connection refused
+//    val request = Http("http://apache.spark.org/") // Service unavailable
     .timeout(connTimeoutMs = 3000, readTimeoutMs = 10000)
 
+    var evalRequest = None: Option[HttpResponse[Array[Byte]]]
+
     try {
-      response.asBytes.code match {
-        case 200 => println("Ok")
-        case 301 =>
-          // "http://fontanka.ru/fontanka.rss"
-          println(s"Page moved: ${response.asString.body}")
+      evalRequest = Some(request.asBytes)
+      evalRequest.get.code match {
+        case 200 => //println("Ok")
+        case 301 => println(s"Page Moved: ${evalRequest.get.toString}")
+          // Better to handle forwarding
+
           System.exit(1)
-        case 404 => println(s"Page not found: ${response.url}"); System.exit(1)
-        case n: Int => println(s"${response.asString}\nResponse code: $n"); System.exit(1)
+        case 404 => println(s"Page Not Found: ${request.url}")
+          System.exit(1)
+        case 503 => println(s"Service Unavailable: ${request.url}")
+          // Better to retry with some strategy
+          System.exit(1)
+        case n: Int => println(s"${evalRequest.get.toString}\nResponse code: $n")
+          System.exit(1)
       }
     } catch {
       case e: UnknownHostException => println(s"Host not found: ${e.getMessage}"); System.exit(1)
-      case e: SocketTimeoutException => println(s"$e URL:${response.url}"); System.exit(1)
-      case e: ConnectException => println(s"$e URL:${response.url}"); System.exit(1)
+      case e: SocketTimeoutException => println(s"$e URL:${request.url}"); System.exit(1)
+      case e: ConnectException => println(s"$e URL:${request.url}"); System.exit(1)
+      case e: MalformedURLException => println(s"$e URL:${request.url}"); System.exit(1)
     }
 
-    val declAttrs: Map[String,String] = response.asString.body
+    val response = evalRequest.getOrElse(throw new RuntimeException())
+    /* All this code has written because in case
+     * WHEN http server didn't return charset for the xml content-type
+     *   AND incoming xml declaration has encoding = "some-encoding"
+     * In this case we use xml declaration encoding
+     * In case XML declaration doesn't have encoding attribute
+     *    we use HTTP response charset if set or ISO-8859-1 as default
+     */
+    val respCharset: String = response.headers.get("content-type").flatMap(_.headOption).flatMap(ct => {
+      HttpConstants.CharsetRegex.findFirstMatchIn(ct).map(_.group(1))
+    }).getOrElse("ISO-8859-1")
+    val declAttrs: Map[String,String] = new String(response.body, respCharset)
       .split("\\?")(1).split(" ").toList
       .filter(_ != "xml")
       .map((x) => {val kv=x.split("="); (kv(0).toLowerCase(),kv(1).replace("\"", "")) })
       .toMap
+/*
+    println(response.contentType.getOrElse("None"))
+    println(response.headers)
+    println(s"HTTP charset:$respCharset\nXML encoding:${declAttrs.get("encoding")}")
+*/
+
     // TODO there should be a way to get encoding from XML declaration
+
     val xml = XML
         .load(new InputStreamReader(
-          new ByteArrayInputStream(response.asBytes.body), declAttrs getOrElse ("encoding", "UTF-8"))
+          new ByteArrayInputStream(response.body), declAttrs getOrElse ("encoding", respCharset/*"UTF-8"*/))
         )
     // TODO transform url with formatter
     // TODO handle XML escape characters like &amp; &quot; &apos; &lt; &gt;
+
     val formatter = new PrettyPrinter(240,4)
 
     val chanNode: NodeSeq = xml \\ "rss" \ "channel"
